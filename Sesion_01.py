7b7880b1eb95ddc50a7e4768f805784873a6affa
seguraarizbeth <seguraarizbeth@gmail.com>
2026-02-10 17:29:53 -0800

    Renovar descripcion

--- Sesion_01.py
+++ Sesion_01.py
@@ -22,3 +22,46 @@ print(float(10%3))
 print(float(25%4))
 print(float(16%2))
 print(float(10 / 3))
+
+#variables
+print("=========VARIABLES========")
+x = 100
+y = 1
+print(x + y)
+
+ventas = 1999991
+print("Nuestras ventas fueron: ", ventas)
+
+is_active = True
+print(is_active)
+
+game_over =False
+print(game_over)
+
+some_string = "Hola soy un string"
+print(some_string)
+
+print("=======Condicionales======")
+edad = 15
+
+if(edad >=18):
+    print("Si puedes entar a el Bar!!!")
+
+else:
+    print("No puedes entrar a el Bar!!!")
+
+    mi_numero = int(input("Cual es el numero que deseas verificar?: "))
+    if mi_numero % 2 == 0:
+        print(f"El numero{mi_numero} es par!")
+    else:
+        print(f"El numero{mi_numero} es impar!!!1")
+
+        def par_impar(numero):
+            if numero % 2 == 0:
+                print(f"El numero{numero} es par!")
+            else:
+                print(f"El numero{numero} es impar!!!")
+
+mi_numero = int(input("Cual es el numero que deseas verificar?: "))
+print(f"El numero que deseas verificar es {mi_numero}")
+print(par_impar(mi_numero))                
