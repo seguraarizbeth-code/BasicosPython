3d63e44716ab009f41a8bb56a5b8a7c5829658fe
seguraarizbeth <seguraarizbeth@gmail.com>
2026-02-12 17:36:15 -0800

    Sesion 3 por 2

--- Sesion_03.py
+++ Sesion_03.py
@@ -15,8 +15,40 @@ print(f"El resultado de la suma de mi lista es:  {resultado}")
 for i in range(2,9):
     print(i)
 
-    mi_lista_2 = ["Lunes", "Martes","Miercoles","Juves","Viernes"]
+    mi_lista_2 = ["Lunes", "Martes","Miercoles","Jueves","Viernes"]
 
     for i in mi_lista_2:
         if i != "Lunes":
             print(f"Feliz {i}!")
+
+#While loop
+i = 0
+
+while i < 5:
+    i += 1
+    if i == 3:
+        continue
+    print (i)
+    if i == 4:
+        break
+
+    else:
+        print("i es ahora mayor o igual 5")
+
+#Practica 2
+#Dada la lista mi_lista_2 = ["lunes,"Martes","Miercoles","jueves","Viernes"]
+#Imprime cada elemento de la lista 3 veces y cuando sea lunes no lo incluyas
+#Pista: Usalos dos tipos loops while y for en el mismo programa para logralo
+#Resultado
+#martes
+#miercoles
+#jueves"
+#viernes
+#martes
+#miercoles
+#jueves"
+#viernes
+#martes
+#miercoles
+#jueves"
+#viernes
